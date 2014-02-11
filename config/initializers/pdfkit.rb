008a48afa97e93f704c7efcfd3d085921770ed95
priyanko <priynko.priyankodey87@gmail.com>
2014-02-11 15:39:16 +0530

    First commit

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -2,9 +2,7 @@
 PDFKit.configure do |config|
 # config.wkhtmltopdf = "#{}"Rails.root.join(‘vendor’, ‘wkhtmltopdf-amd64’).to_s if Rails.env.production?
 
-  wkhtmltopdf_executable = 'wkhtmltopdf-amd64'
-  config.wkhtmltopdf = Rails.root.join('vendor', 'bin', wkhtmltopdf_executable).to_s
-
+  
 
 # Rails.root.join(‘vendor’, ‘wkhtmltopdf-amd64’).to_s if Rails.env.production?
   config.wkhtmltopdf = 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
@@ -12,6 +10,10 @@ PDFKit.configure do |config|
     :page_size => 'Legal',
     :print_media_type => true
   }
+  
+  wkhtmltopdf_executable = 'wkhtmltopdf-amd64'
+  config.wkhtmltopdf = Rails.root.join('vendor', 'bin', wkhtmltopdf_executable).to_s
+
   # Use only if your external hostname is unavailable on the server.
   config.root_url = "http://localhost" 
 end
